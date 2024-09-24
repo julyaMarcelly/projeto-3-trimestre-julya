5b728ac91a1f591fca64eef98438158ff51910d3
julyaMarcelly <julya.rosario@escola.pr.gov.br>
2024-09-24 17:06:27 -0300

    Add files via upload
    webp

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,28 @@
+criaCartao(
+    'História',
+    'Qual foi o papel de Napoleão Bonaparte na história da França e da Europa?',
+    'Ele foi um general que se tornou o lider da França após a revolão Francesa, instaurabdo o Cunsuado e depois e proclamou imperador em 1804 '
+)
+
+criaCartao(
+    'Arte',
+    'Qual a importância do imprisionismo na história da arte?',
+    'O Ipressionismo foi importante na história da arte poque roumpeu com as tradições acadêmicas, focando na captura de momentos e na representação da luz e das cres de mnaneira mais espontẫnea'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo e qual é sua capital?',
+    ' É a Ásia e suas capital é a China'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
