46002ec6221d65647415c240fbba29cb3ccdf6c3
julyaMarcelly <julya.rosario@escola.pr.gov.br>
2024-10-01 15:08:14 -0300

    Add files via upload

---
+++ projeto-3-trimestre-julya-main/3 trimes julya/perguntas.js
@@ -0,0 +1,28 @@
+criaCartao(
+    'Historia',
+    'Qual foi o papel de Napoleão Bonaparte na historia da França e da Europa?',
+    '   Ele foi um general que e tornou lider da França após a Revolução Francesa, ele instaurou e depois proclamou imperador em 1804'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo e qual a sua capital?',
+    'Ásia e sua capital é em Pequim, China'
+)
+
+criaCartao(
+    'Arte',
+    'O que arte?',
+    'È uma manifestação humana que caracteriza pel criatividade de exprssão'
+)
+criaCartao(
+    'Arte',
+    'O que arte?',
+    ''
+)
